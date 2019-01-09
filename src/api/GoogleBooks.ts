0cb6960fe5c753b4e0148ba441fa34604521f019
Konnor Welsch <konnor.welsch@eagles.ewu.edu>
2019-01-09 00:11:43 -0800

    Get successful api search

---
+++ src/api/GoogleBooks.ts
@@ -0,0 +1,50 @@
+import axios, { AxiosResponse } from 'axios';
+import { Book, BookSearchResult } from '../models'
+
+const key = '';
+
+export const searchBooks = (query: string, page: number, pageSize: number): Promise<Book[]> => (
+  new Promise((resolve, reject) => {
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${getStartIndex(page, pageSize)}&maxResults=${pageSize}&key=${key}`)
+      .then(({ data }: AxiosResponse) => {
+        const results: BookSearchResult = data;
+        const books = results.items.map(item => item.volumeInfo);
+        resolve(books);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  })
+);
+
+export const advancedSearch = (page: number, pageSize: number, title?: string, author?: string, isbn?: string): Promise<Book[]> => (
+  new Promise((resolve, reject) => {
+    const query = buildParamString(title, author, isbn);
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&startIndex=${getStartIndex(page, pageSize)}&maxResults=${pageSize}&key=${key}`)
+      .then(({ data }: AxiosResponse) => {
+        const results: BookSearchResult = data;
+        const books = results.items.map(item => item.volumeInfo);
+        resolve(books);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  })
+);
+
+const getStartIndex = (page: number, pageSize: number) => (page - 1) * pageSize;
+
+// See https://developers.google.com/books/docs/v1/using#PerformingSearch -- there's also stuff like publisher
+const buildParamString = (title?: string, author?: string, isbn?: string) => {
+  let query = '';
+  if (title) {
+    query += `+intitle:${title}`;
+  }
+  if (author) {
+    query += `+inauthor:${author}`;
+  }
+  if (isbn) {
+    query += `+isbn:${isbn}`;
+  }
+  return query;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
+export { BookViewer } from './BookViewer';
 export { Footer } from './Footer';
 export { Header } from './Header';
 export { MediaList } from './MediaList';
+export { PaginatedBookList } from './PaginatedBookList';
